7200a8073871f3ee2eaf4382a63ebe8e27e3c76e
niksami <niksami@gmail.com>
2024-02-07 09:20:41 +0100

    Modified HomePage to use Selenium FindBy annotation for identifying elements on page.

--- src/src/main/java/org/example/pages/HomePage.java
+++ src/src/main/java/org/example/pages/HomePage.java
@@ -1,17 +1,30 @@
 package org.example.pages;
 
-import org.openqa.selenium.By;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static java.time.Duration.ofSeconds;
+import static org.openqa.selenium.support.PageFactory.initElements;
 
 public class HomePage {
-    protected WebDriver driver;
+    protected static WebDriver driver;
+
+    @FindBy(xpath = "//*[@id=\"hero-content\"]/div[3]/div[1]/div[4]/div/div/a/div[1]")
+    protected static WebElement welcomeText;
 
     public HomePage(WebDriver driver) {
-        this.driver = driver;
+        HomePage.driver = driver;
     }
 
+    private void reInitiatePage() {
+        initElements(new AppiumFieldDecorator(driver, ofSeconds(10)), this);
+    }
+
+
     public boolean isWelcomeDisplayed() {
-        String text = "//*[@id=\"hero-content\"]/div[3]/div[1]/div[4]/div/div/a/div[1]";
-        return driver.findElement((By.xpath(text))).isDisplayed();
+        reInitiatePage();
+        return welcomeText.isDisplayed();
     }
 }
